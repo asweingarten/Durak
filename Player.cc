723a3e5c72e4dc60251cc86a4f7d3ba1070b5de1
Ariel Weingarten <asweingarten@gmail.com>
2013-12-24 12:00:08 -0500

    Constructor and action loop

--- Player.cc
+++ Player.cc
@@ -1,12 +1,15 @@
 #include "Player.cc"
 
-Player::Player()
+Player::Player( unsigned int id, Table &table ) :
+				table( &table ), id( id ), hand( new CardCollection() )
 {
 
 }
 
 void Player::main()
 {
+	table->pickUpCards( hand );
+
 	while ( true )
 	{
 		break;
